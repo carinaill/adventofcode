52f46761a00c279be363bec2e04a0e1a0ab7499a
Carina <cillgen@cgs-online.de>
2023-09-11 15:08:38 +0200

    implemented day 1, the second part of the advent of code

--- src/main/java/de/illgen/adventofcode/Day1.java
+++ src/main/java/de/illgen/adventofcode/Day1.java
@@ -2,6 +2,7 @@ package de.illgen.adventofcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Day1 {
@@ -9,22 +10,27 @@ public class Day1 {
 	public static void main(String[] args) {
 		String data = FileReader.readFile("day1");
 		List<String> lines = new ArrayList<>(Arrays.asList(data.split("\r?\n")));
-		
+
 		List<Integer> allcalories = parse(lines);
 		List<Integer> caloriesPerElf = sumUp(allcalories);
-		int mostCalories = findMax(caloriesPerElf);
-		
+		caloriesPerElf.sort(Comparator.reverseOrder());
+
+		int mostCalories = part1(caloriesPerElf);
 		System.out.println(mostCalories);
+		int topThree = part2(caloriesPerElf);
+		System.out.println(topThree);
 	}
 
-	private static int findMax(List<Integer> caloriesPerElf) {
-		int max = 0;
-		for (Integer calories : caloriesPerElf) {
-			if (calories > max) {
-				max = calories;
-			}
+	private static int part1(List<Integer> caloriesPerElf) {
+		return caloriesPerElf.get(0);
+	}
+
+	private static int part2(List<Integer> caloriesPerElf) {
+		int topThree = 0;
+		for (int i = 0; i < 3; i++) {
+			topThree += caloriesPerElf.get(i);
 		}
-		return max;
+		return topThree;
 	}
 
 	private static List<Integer> sumUp(List<Integer> allcalories) {
