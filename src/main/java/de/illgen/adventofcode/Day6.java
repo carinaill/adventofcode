f2aebd8f57947aeba207cda051d5dc1434537b1b
Carina <cillgen@cgs-online.de>
2023-09-21 15:17:34 +0200

    improved day6 advent of code

--- src/main/java/de/illgen/adventofcode/Day6.java
+++ src/main/java/de/illgen/adventofcode/Day6.java
@@ -3,20 +3,20 @@ package de.illgen.adventofcode;
 public class Day6 {
 
 	public static void main(String[] args) {
-		String data = FileReader.readFile("day6");
+		String data = FileReader.readFile("day6example1");
 		
 		int index = findIndex(data, 3);
-		System.out.println(index+1);
+		System.out.println(index);
 		
 		int index2 = findIndex(data, 13);
-		System.out.println(index2+1);
+		System.out.println(index2);
 	}
 	
 	private static int findIndex(String data, int last) {
 		for (int i = last; i < data.length(); i++) {
 			
 			if(checkLast(data, i, last)) {
-				return i;
+				return i+1;
 			}		
 		}
 		return -1;
@@ -31,7 +31,5 @@ public class Day6 {
 			}
 		}
 		return true;
-	}
-	
-	
+	}	
 } 
